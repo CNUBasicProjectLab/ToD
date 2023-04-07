0025391f750c8df892906db2cb6094c897d37298
YimKeul <leesjun29@gmail.com>
2023-04-07 11:19:31 +0900

    Initial Commit

---
+++ ToD/ToDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDApp.swift
+//  ToD
+//
+//  Created by yimkeul on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct ToDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
