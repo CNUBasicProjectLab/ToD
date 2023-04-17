aa3a033ea7c9b39b250831df4036fe768ee008fe
YimKeul <leesjun29@gmail.com>
2023-04-18 03:02:51 +0900

    chore : clean dir

---
+++ ToD/AfterChar/ToDoView.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoView.swift
+//  ToD
+//
+//  Created by yimkeul on 2023/04/18.
+//
+
+import SwiftUI
+
+struct ToDoView: View {
+    var body: some View {
+        Text("ToDoView")
+    }
+}
+
+struct ToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoView()
+    }
+}
