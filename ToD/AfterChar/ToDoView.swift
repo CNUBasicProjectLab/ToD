e7836727365b7db9c4eea6e76a37bfc1650761c2
YimKeul <leesjun29@gmail.com>
2023-05-10 15:06:15 +0900

    feat : edit ToDoView

--- ToD/AfterChar/ToDoView.swift
+++ ToD/AfterChar/ToDoView.swift
@@ -11,21 +11,137 @@ import SwiftUI
 struct ToDoView: View {
     @AppStorage("isChar") var isChar: Bool = true
     
-
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("ToDoView")
+                ScrollView{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(height: 280)
+//                        .padding()
+                        .foregroundColor(Color.secondary)
+                        .shadow(color: .gray, radius: 2, x: 0, y: 3)
+                    Text("투디 이미지 미리보기")
+                }
+                Divider()
+                Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                    /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                }.pickerStyle(.segmented)
+                    .padding()
+                
+                Spacer()
+                    VStack
+                    {
+                        HStack{
+                            Image("Daily")
+                            VStack(alignment : .leading){
+                                Text("Title")
+                                Text("SubTitle").font(.caption)
+                            }.padding(.leading)
+                            Spacer()
+                            Image(systemName: "checkmark.circle").foregroundColor(.green)
+                            
+                        }
+                        .padding()
+                        
+                        
+                        
+                        
+                        
+                        
+                        HStack{
+                            Image("Week")
+                            VStack(alignment : .leading){
+                                Text("Title")
+                                Text("SubTitle").font(.caption)
+                            }.padding(.leading)
+                            Spacer()
+                            Image(systemName: "checkmark.circle").foregroundColor(.green)
+                            
+                        }
+                        .padding()
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        HStack{
+                            Image("Month")
+                            VStack(alignment : .leading){
+                                Text("Title")
+                                Text("SubTitle").font(.caption)
+                            }.padding(.leading)
+                            Spacer()
+                            Image(systemName: "checkmark.circle").foregroundColor(.green)
+                            
+                        }
+                        .padding()
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        HStack{
+                            Image("Year")
+                            VStack(alignment : .leading){
+                                Text("Title")
+                                Text("SubTitle").font(.caption)
+                            }.padding(.leading)
+                            Spacer()
+                            Image(systemName: "checkmark.circle").foregroundColor(.green)
+                            
+                        }
+                        .padding()
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        HStack{
+                            Image("Mission")
+                            VStack(alignment : .leading){
+                                Text("Title")
+                                Text("SubTitle").font(.caption)
+                            }.padding(.leading)
+                            Spacer()
+                            Image(systemName: "checkmark.circle").foregroundColor(.green)
+                            
+                        }
+                        .padding()
+                    }
+                    
+                    
+                    
+                }.padding()
                 Button {
                     isChar = false
                 } label: {
                     Text("다시 false로")
                 }
-
+                
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
-                    Text("투디")
+                    HStack{
+                        Image("Logo")
+                            .resizable()
+                            .frame(width: 48,height: 48)
+                        Text("투디")
+                            .font(.system(size : 24, weight: .semibold))
+                    }
+                    
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
